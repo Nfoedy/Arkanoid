0f4ee581b4fc97072233af3f5bd71906b4e2547c
RiccardoConforti <riccardoconforti96@gmail.com>
2026-05-27 16:35:37 +0200

    Add win32 window system

---
+++ ArkanoidDX11/main.cpp
@@ -0,0 +1,166 @@
+#define WIN32_LEAD_AND_MEAN
+
+#include <windows.h> // Libreria per le API Win32
+
+
+// Funzione che Windows chiama ogni volta che succede qualcosa alla finestra
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	// hwnd = handle della finestra che ha ricevuto il messaggio
+	// msg = tipo di messaggio ricevuto da Windows
+	// wParam = parametro aggiuntivo al messaggio 
+	// lParma = altro parametro aggiuntivo al messaggio
+
+	switch (msg)
+	{
+
+		// Alla distruzione della finestra
+	case WM_DESTROY:
+		PostQuitMessage(0);     // Dice di uscre dal message loop
+		return 0;
+
+		// Quando viene premuto un tasto della tastiera
+	case WM_KEYDOWN:
+		// Se viene premuto ESC
+		if (wParam == VK_ESCAPE)
+		{
+			// Chiudo il programma
+			PostQuitMessage(0);
+		}
+		return 0;
+	}
+
+	// Tutti i messaggi che non gestisco vengono passati alla gestione di default di Windows
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+
+// è il main(), ma per programmi Win32 con finestra
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+{
+	// hInstance = Handle dell'istanza corrente del sistema
+	// hPrevInstance = Non usato nelle app moderne
+	// pCmdLine = Argomenti da command line
+	// nCmdShow = Come mostrare la finestra
+
+	// dim
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	// Titilo della finestra
+	const wchar_t* CLASS_NAME = L"ArkanoidDX11WindowClass";
+
+
+    /*
+        1. Registrazione della classe finestra
+    */
+
+    // Contiene le info per dire a Windows che finestra voglio creare
+    WNDCLASSEX wc = {};
+
+    // Dimensione della struttura.
+    wc.cbSize = sizeof(WNDCLASSEX);
+
+    // Stile della finestra.
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+
+    // Funzione che gestisce i messaggi della finestra.
+    wc.lpfnWndProc = WndProc;
+
+    // Istanza dell'applicazione.
+    wc.hInstance = hInstance;
+
+    // Cursore standard della freccia.
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    // Nome della classe finestra.
+    wc.lpszClassName = CLASS_NAME;
+
+    // Registriamo la classe finestra presso Windows.
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(nullptr, L"Errore registrazione finestra!", L"Errore", MB_OK);
+        return -1;
+    }
+
+
+    /*
+        2. Calcolo dimensione reale della finestra
+    */
+
+
+    RECT windowRect = { 0, 0, WIDTH, HEIGHT };
+
+    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+
+    /*
+        3. Creazione della finestra
+    */
+
+    HWND hwnd = CreateWindowEx(
+        0,                                      // stile esteso, per ora nessuno
+        CLASS_NAME,                             // classe finestra registrata prima
+        L"Arkanoid DX11",                       // titolo visibile della finestra
+        WS_OVERLAPPEDWINDOW,                    // finestra standard con bordi e titolo
+        CW_USEDEFAULT,                          // posizione X automatica
+        CW_USEDEFAULT,                          // posizione Y automatica
+        windowRect.right - windowRect.left,     // larghezza totale finestra
+        windowRect.bottom - windowRect.top,     // altezza totale finestra
+        nullptr,                                // finestra padre, non serve
+        nullptr,                                // menu, non serve
+        hInstance,                              // istanza applicazione
+        nullptr                                 // dati extra, non servono
+    );
+
+    // Se hwnd è nullptr, la finestra non è stata creata correttamente.
+    if (!hwnd)
+    {
+        MessageBox(nullptr, L"Errore registrazione finestra!", L"Errore", MB_OK);
+
+        return -1;
+    }
+
+
+    /*
+        4. Mostrare la finestra
+    */
+
+    // Mostra la finestra a schermo.
+    ShowWindow(hwnd, nCmdShow);
+
+    // Forza un primo aggiornamento della finestra.
+    UpdateWindow(hwnd);
+
+
+    /*
+        5. Message loop
+    */
+
+    // MSG contiene i messaggi che Windows invia alla nostra applicazione.
+    MSG msg = {};
+
+    // Il programma continua finché non riceve WM_QUIT.
+    while (msg.message != WM_QUIT)
+    {
+       
+        // PM_REMOVE significa: se trova un messaggio, rimuovilo dalla coda.
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            // Traduce alcuni messaggi da tastiera.
+            TranslateMessage(&msg);
+
+            // Manda il messaggio alla WndProc.
+            DispatchMessage(&msg);
+        }
+
+
+
+    }
+
+
+    // Quando usco dal loop, ritorno il codice di uscita.
+    return static_cast<int>(msg.wParam);
+
+
+}
