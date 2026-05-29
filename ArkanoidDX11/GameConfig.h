80dbb51c4dc53b01d6a7e96c2f7bea425d130325
RiccardoConforti <riccardoconforti96@gmail.com>
2026-05-29 21:31:50 +0200

    fix powerup dropchance

--- ArkanoidDX11/GameConfig.h
+++ ArkanoidDX11/GameConfig.h
@@ -66,7 +66,7 @@ namespace GameConfig
     // PowerUps / Malus - per dopo
     // ============================================================
 
-    constexpr float PowerUpDropChance = 0.15f;
+    constexpr float PowerUpDropChance = 0.20f;
     constexpr float PowerUpFallSpeed = 0.45f;
     constexpr float PowerUpSize = 0.06f;
     constexpr float PowerUpDuration = 6.0f;
